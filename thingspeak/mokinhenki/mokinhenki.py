22e6f2f1c2d0b38a11cf2d98980f4ed789e05ea0
Jussi Korpela <jussitapiokorpela@gmail.com>
2017-04-04 22:36:18 +0300

    Error handling.

--- thingspeak/mokinhenki/mokinhenki.py
+++ thingspeak/mokinhenki/mokinhenki.py
@@ -140,7 +140,8 @@ def sendData(url, key, temp, hum):
 		"""
         
     except:
-        log = log + 'sendData error. Reason: ' + sys.exc_info()[0]
+        exctype, value = sys.exc_info()[:2]
+        log = log + 'sendData error. Reason: ' + str(value)
 
     print log
     
@@ -174,7 +175,8 @@ def sendGMail(msg, google_app_token):
         log = log + 'Send mail notification.'
     
     except:
-        log = log + 'sendData error. Reason: ' + sys.exc_info()[0]
+        exctype, value = sys.exc_info()[:2]
+        log = log + 'sendData error. Reason: ' + str(value)
 
     print log
     with open(LOGFILE, 'a') as file:
@@ -217,92 +219,100 @@ def main():
 
     while True:
 
-        # Take picture based on PIR
-        if (GPIO.input(PIN_PIR)):
-            #print("Taking a picture...")
-            #camera.start_preview()
-            
-            time.sleep(0.2) #wait for the object to move fully into frame
-            
-            ts = time.time()
-            tstr = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H-%M-%S')
-            fname = os.path.join(OUTPATH_FIG, "pirimage_%s.jpg" % (tstr))
-            
-            camera.capture(fname)
-
-            # Create email
-            msg = createMultipartEMail(from_email, to_email,
-                    'Mokinhenki tiedottaa: liiketunnistin lauennut',
-                    'Liiketunnistimeni laukesi. Ohessa kuva.')
-            
-            # Attach image
-            fp = open(fname, 'rb')
-            img = MIMEImage(fp.read())
-            fp.close()
-            msg.attach(img)
-
-            # Send using gmail
-            status = sendGMail(msg, app_token)
-
-            #time.sleep(3) #sleep until PIR is ready again
-            #camera.stop_preview()
-
-
-        # Take picture based on time of day
-        curts = time.localtime()
-        if (curts[3]==12) & (curts[4]==0) & (day_pic_taken != True):
-            ts = time.time()
-            tstr = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H-%M-%S')
-            fname = os.path.join(OUTPATH_FIG, "dayimage_%s.jpg" % (tstr))
-            camera.capture(fname)
-            day_pic_taken = True
-
-        if (curts[3]==12) & (curts[4]==1):
-            day_pic_taken = False #reset flag
-        
-        
-        # Log temperature and humidity
-        if (time.time() - last_temphum_ts) > TEMPHUM_INTERVAL_SEC:
-            
-            #[temp, hum] = grovepi.dht(4, 1) #D4 input, the white version          
-            
-            # Try to grab a sensor reading.  Use the read_retry method which will retry up
-            # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-            hum_old = hum
-            temp_old = temp
-            hum, temp = Adafruit_DHT.read_retry(DHT_SENSOR, PIN_DHT22)
+        try:
+            # Take picture based on PIR
+            if (GPIO.input(PIN_PIR)):
+                #print("Taking a picture...")
+                #camera.start_preview()
                 
-            # Note that sometimes you won't get a reading and
-            # the results will be null (because Linux can't
-            # guarantee the timing of calls to read the sensor).
-            # If this happens try again!
-            if hum is None or temp is None:
-                hum = hum_old
-                temp = temp_old
+                time.sleep(0.2) #wait for the object to move fully into frame
+                
+                ts = time.time()
+                tstr = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H-%M-%S')
+                fname = os.path.join(OUTPATH_FIG, "pirimage_%s.jpg" % (tstr))
+                
+                camera.capture(fname)
+
+                # Create email
+                msg = createMultipartEMail(from_email, to_email,
+                        'Mokinhenki tiedottaa: liiketunnistin lauennut',
+                        'Liiketunnistimeni laukesi. Ohessa kuva.')
+                
+                # Attach image
+                fp = open(fname, 'rb')
+                img = MIMEImage(fp.read())
+                fp.close()
+                msg.attach(img)
+
+                # Send using gmail
+                status = sendGMail(msg, app_token)
+
+                #time.sleep(3) #sleep until PIR is ready again
+                #camera.stop_preview()
+
+
+            # Take picture based on time of day
+            curts = time.localtime()
+            if (curts[3]==12) & (curts[4]==0) & (day_pic_taken != True):
+                ts = time.time()
+                tstr = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H-%M-%S')
+                fname = os.path.join(OUTPATH_FIG, "dayimage_%s.jpg" % (tstr))
+                camera.capture(fname)
+                day_pic_taken = True
+
+            if (curts[3]==12) & (curts[4]==1):
+                day_pic_taken = False #reset flag
             
-            if all(x is not None for x in [temp, hum]) & (-40 < temp) & (temp < 50) & (0 < hum) & (hum < 100):
-                            print("temp = %.02f C humidity =%.02f%%"%(temp, hum))
-                            sendData(THINGSPEAKURL, THINGSPEAKKEY, temp, hum)
-                            sys.stdout.flush()
+            
+            # Log temperature and humidity
+            if (time.time() - last_temphum_ts) > TEMPHUM_INTERVAL_SEC:
+                
+                #[temp, hum] = grovepi.dht(4, 1) #D4 input, the white version          
+                
+                # Try to grab a sensor reading.  Use the read_retry method which will retry up
+                # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+                hum_old = hum
+                temp_old = temp
+                hum, temp = Adafruit_DHT.read_retry(DHT_SENSOR, PIN_DHT22)
+                    
+                # Note that sometimes you won't get a reading and
+                # the results will be null (because Linux can't
+                # guarantee the timing of calls to read the sensor).
+                # If this happens try again!
+                if hum is None or temp is None:
+                    hum = hum_old
+                    temp = temp_old
+                
+                if all(x is not None for x in [temp, hum]) & (-40 < temp) & (temp < 50) & (0 < hum) & (hum < 100):
+                                print("temp = %.02f C humidity =%.02f%%"%(temp, hum))
+                                sendData(THINGSPEAKURL, THINGSPEAKKEY, temp, hum)
+                                sys.stdout.flush()
 
-                            # update history records: pop last element and add new to position 0
-                            temp_hist.pop()
-                            temp_hist.insert(0, temp)
-                            hum_hist.pop()
-                            hum_hist.insert(0, hum)
+                                # update history records: pop last element and add new to position 0
+                                temp_hist.pop()
+                                temp_hist.insert(0, temp)
+                                hum_hist.pop()
+                                hum_hist.insert(0, hum)
 
-                            # send email warning
-                            if ((np.median(temp_hist) < TEMP_ALARM_TH) | (np.median(hum_hist) > HUM_ALARM_TH)) & ((time.time() - last_temphum_email_ts) > TEMPHUM_ALARM_INTERVAL_SEC):
-                                msg = createMultipartEMail(from_email, to_email,
-                                'Mokinhenki tiedottaa: liian kylmaa tai kosteaa',
-                                'Majassani on liian kylmaa tai kosteaa. Lampotila on %2.1f astetta ja suht. kosteus %2.1f prosenttia. Tee jotain.' % (temp, hum) )
-                                status = sendGMail(msg, app_token)
-                                last_temphum_email_ts = time.time()
+                                # send email warning
+                                if ((np.median(temp_hist) < TEMP_ALARM_TH) | (np.median(hum_hist) > HUM_ALARM_TH)) & ((time.time() - last_temphum_email_ts) > TEMPHUM_ALARM_INTERVAL_SEC):
+                                    msg = createMultipartEMail(from_email, to_email,
+                                    'Mokinhenki tiedottaa: liian kylmaa tai kosteaa',
+                                    'Majassani on liian kylmaa tai kosteaa. Lampotila on %2.1f astetta ja suht. kosteus %2.1f prosenttia. Tee jotain.' % (temp, hum) )
+                                    status = sendGMail(msg, app_token)
+                                    last_temphum_email_ts = time.time()
 
-                            last_temphum_ts = time.time()
+                                last_temphum_ts = time.time()
 
 
-        time.sleep(0.5) #for the while loop
+            time.sleep(0.5) #for the while loop
+
+        except:
+            exctype, value = sys.exc_info()[:2]
+            log = 'Loop error: ' + str(value)
+            print(log)
+            with open(LOGFILE, 'a') as file:
+                file.write(log + '\n')
 
 
 if __name__=="__main__":
